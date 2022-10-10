c4ea5648009cda4ae8db98a471bbbff5a50c3042
AspirantDev <dormantdeveloper@gmail.com>
2022-10-10 15:45:59 +0100

    Changed string

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -6,7 +6,7 @@ namespace HelloCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello c#!");
         }
     }
 }
